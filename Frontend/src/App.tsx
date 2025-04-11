f2e0e0e8314617a0178780e0aa48e1e018a34a8f
Uday_Prabhas <udayprabhas005@gmail.com>
2025-04-11 23:59:37 +0530

    landing

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Landing from './Landing'; // Make sure the file exists and is correctly named
+
+function App() {
+  return (
+    <>
+      <Landing />
+    </>
+  );
+}
+
+export default App;
